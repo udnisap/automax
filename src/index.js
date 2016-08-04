0c2adc604173380f7b26731a2769ce68cfb78079
Pasindu Perera <perera.pasindu@gmail.com>
2016-08-05 00:33:30 +0530

    refactor(framework): es6 arrow function use and npm start script

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 #!/usr/bin/env node
-"use strict";
 require("babel-core/register");
 var automax = require('./automax')({});
 var fs = require('fs');
 var _ = require('lodash');
 var path = require('path');
 
-//Loop through plugins directory
+// Loop through plugins directory
 _.chain(fs.readdirSync(path.join(__dirname, 'plugins')))
-  .filter(function(file){
-    return /.+\.js/.exec(file);
-  })
-  .map(function(file){
-    return file.replace('.js', '');
-  })
-  .each(function(plugin){
-    automax.addPlugin(_.kebabCase(plugin), path.join(__dirname, 'plugins', plugin));
-  })
+  .filter(fileName => /.+\.js/.exec(fileName))
+  .map(file => file.replace('.js', ''))
+  .each(plugin => automax.addPlugin(_.kebabCase(plugin), path.join(__dirname, 'plugins', plugin)))
   .value();
 
 
