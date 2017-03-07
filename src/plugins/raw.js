0359a414ddd5372b3d8724aacf02405e7c6305bb
Pasindu Perera <perera.pasindu@gmail.com>
2017-03-07 15:25:32 -0800

    feat(framework): added better support for commandline argument parsing

--- src/plugins/raw.js
+++ src/plugins/raw.js
@@ -1,22 +1,27 @@
 import _ from 'lodash';
-module.exports = function ({vorpal, driver: {current: browser}}){
-  function registerMethod(property){
-    vorpal
-      .command(`${property} [params...]`, 'Refer http://webdriver.io/api')
-      .action(function *({params = []}){
-        const cleanedParams = params.map(i => i.replace ? i.replace('\\','') : i)
-        const res = yield Promise.resolve(browser[property].apply(this, cleanedParams))
-        if (res.value){
-          vorpal.log(res.value);
-        }else {
-          vorpal.log(res);
-        }
-      });
-  }
 
+function registerMethod(vorpal, browser, property) {
+  vorpal
+    .command(`${property} [params...]`, 'Refer http://webdriver.io/api')
+    .parse(cmd => cmd
+      .split(' ')
+      .map(encodeURIComponent)
+      .join(' '))
+    .action(async ({ params = [] }) => {
+      const cleanedParams = params.map(decodeURIComponent);
+      const res = await browser[property](...cleanedParams);
+      if (res.value) {
+        vorpal.log(res.value);
+      } else {
+        vorpal.log(res);
+      }
+    });
+}
+
+module.exports = ({ vorpal, driver: { current: browser } }) => {
   const methods = _.keysIn(browser);
 
-  methods.forEach(property => {
-    registerMethod(property);
-  })
+  methods.forEach((property) => {
+    registerMethod(vorpal, browser, property);
+  });
 };
